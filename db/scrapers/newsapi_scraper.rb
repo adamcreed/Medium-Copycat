8e7e658c7441616775873ddd18bcf688f0da22d0
Adam Reed <kusa.xisa@gmail.com>
2017-02-17 17:17:57 -0500

    Added article api scraper

--- db/scrapers/newsapi_scraper.rb
+++ db/scrapers/newsapi_scraper.rb
@@ -1,9 +1,6 @@
-require 'dotenv/load'
 require 'httparty'
 require 'nokogiri'
-require 'json'
-require 'pry'
-require 'csv'
+
 
 api_key = ENV['NEWSAPI_KEY']
 sources_page = HTTParty.get 'https://newsapi.org/sources'
@@ -20,14 +17,16 @@ news_sources.each do |news_source|
   stories = HTTParty.get "https://newsapi.org/v1/articles?source=#{news_source}&sortBy=top&apiKey=#{api_key}"
 
   if stories['status'] == 'ok'
-  stories['articles'].each do |story|
-    Article.create(
-      author: story['author'],
-      title: story['title'],
-      description: story['description'],
-      url: story['url'],
-      url_to_image: story['urlToImage'],
-      published_at: story['publishedAt'][date_regex]
-    )
+    stories['articles'].each do |story|
+      story['publishedAt'] = story['publishedAt'] || '1999-09-09'
+      Article.create(
+        author: story['author'],
+        title: story['title'],
+        description: story['description'],
+        url: story['url'],
+        url_to_image: story['urlToImage'],
+        published_at: story['publishedAt'][date_regex]
+      )
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-require 'httparty'
-require 'nokogiri'
+Dir["./db/scrapers/*.rb"].each { |file| require file }
 
-api_key = ENV['NEWSAPI_KEY']
-sources_page = HTTParty.get 'https://newsapi.org/sources'
-# regex captures source name without leading slash and ending '-api'
-source_regex = /\/([\w-]+)-api/
-# regex captures the date from a time stamp
-date_regex = /(\d+[\/-]\d+[\/-]\d+)/
-
-news_sources = Nokogiri::HTML(sources_page).css('.source').map do |news_source|
-  news_source['href'][source_regex, 1]
-end
-
-news_sources.each do |news_source|
-  stories = HTTParty.get "https://newsapi.org/v1/articles?source=#{news_source}&sortBy=top&apiKey=#{api_key}"
-
-  if stories['status'] == 'ok'
-    stories['articles'].each do |story|
-      story['publishedAt'] = story['publishedAt'] || '1999-09-09'
-      Article.create(
-        author: story['author'],
-        title: story['title'],
-        description: story['description'],
-        url: story['url'],
-        url_to_image: story['urlToImage'],
-        published_at: story['publishedAt'][date_regex]
-      )
-    end
-  end
-end
+# byebug
