5c3919240ac786f9d1e5c7dda0afad4053e2f1aa
Adam Reed <kusa.xisa@gmail.com>
2017-02-19 14:22:47 -0500

    Updated CORS

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,12 @@
 class SearchController < ApplicationController
+  def articles
+    page = params.fetch 'page', 1
+    per = params.fetch 'per_page', 15
+    q = params.fetch 'q', ''
+    @articles = Article.where("").page(page).per(per).map do |article|
+      article_with_stats(article)
+    end
+
+    render json: @articles
+  end
 end
