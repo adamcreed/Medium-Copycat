01519fab14accabc338be5dab331e5e006327b35
Adam Reed <kusa.xisa@gmail.com>
2017-02-19 12:17:58 -0500

    Added comma

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,18 +50,18 @@ class ArticlesController < ApplicationController
     end
 
     def article_with_stats(article)
-    favorite_count = Mark.where('article_id = ? AND favorited = true', article.id).count
-    bookmark_count = Mark.where('article_id = ? AND bookmarked = true', article.id).count
-    {
-      article: article,
-      favorites: {
-        count: favorite_count,
-        favorited: not(favorite_count.zero?),
-        bookmarked: not(bookmark_count.zero?)
-      },
-      number_of_comments: Comment.where(article_id: article.id).count
-      total_pages: Article.page(1).total_pages
-    }
+      favorite_count = Mark.where('article_id = ? AND favorited = true', article.id).count
+      bookmark_count = Mark.where('article_id = ? AND bookmarked = true', article.id).count
+      {
+        article: article,
+        favorites: {
+          count: favorite_count,
+          favorited: not(favorite_count.zero?),
+          bookmarked: not(bookmark_count.zero?)
+        },
+        number_of_comments: Comment.where(article_id: article.id).count,
+        total_pages: Article.page(1).total_pages
+      }
     end
 
     # Only allow a trusted parameter "white list" through.
